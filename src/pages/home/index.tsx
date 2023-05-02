87d8ad5826e38741a408e3de7099dfc88d8ded51
Kennedy <97395149+KennedyReisz@users.noreply.github.com>
2023-05-02 17:24:13 -0300

    home created structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,13 @@ const Home = () => {
     <>
       <Header />
       <main className={s.homePage}>
-        <h1>Hello World</h1>
+        <h1 className={s.homeMainText}>Explore the world with a smile</h1>
+        <p className={s.mainTextDescription}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringilla urna,
+          porttitor rhoncus dolor purus non enim praesent elementum facilisis
+          leo,
+        </p>
       </main>
     </>
   );
